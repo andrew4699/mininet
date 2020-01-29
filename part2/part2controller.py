0c77f4746784d0cd2a8eb830935d35b4962c63db
Andrew Guterman <andrew.guterman1@gmail.com>
2020-01-29 13:57:26 -0800

    fix typo, normal handler for arp/icmp

--- part2/part2controller.py
+++ part2/part2controller.py
@@ -24,17 +24,17 @@ class Firewall (object):
 
     #add switch rules here
     match = of.ofp_match()
-    match.new_proto = pkt.ipv4.ICMP_PROTOCOL
+    match.nw_proto = pkt.ipv4.ICMP_PROTOCOL
     fm = of.ofp_flow_mod()
     fm.match = match
-    # fm.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+    fm.actions.append(of.ofp_action_output(port = of.OFFP_NORMAL))
     self.connection.send(fm)
 
     match = of.ofp_match()
-    match.new_proto = pkt.arp.REQUEST
+    match.nw_proto = pkt.arp.REQUEST
     fm = of.ofp_flow_mod()
     fm.match = match
-    # fm.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+    fm.actions.append(of.ofp_action_output(port = of.OFFP_NORMAL))
     self.connection.send(fm)
 
     fm = of.ofp_flow_mod()
