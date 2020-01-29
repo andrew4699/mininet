7e36bcdf130ceea090daba8211f39d19e7d15398
Andrew Guterman <andrew.guterman1@gmail.com>
2020-01-29 14:00:00 -0800

    fix another typo

--- part2/part2controller.py
+++ part2/part2controller.py
@@ -27,14 +27,14 @@ class Firewall (object):
     match.nw_proto = pkt.ipv4.ICMP_PROTOCOL
     fm = of.ofp_flow_mod()
     fm.match = match
-    fm.actions.append(of.ofp_action_output(port = of.OFFP_NORMAL))
+    fm.actions.append(of.ofp_action_output(port = of.OFPP_NORMAL))
     self.connection.send(fm)
 
     match = of.ofp_match()
     match.nw_proto = pkt.arp.REQUEST
     fm = of.ofp_flow_mod()
     fm.match = match
-    fm.actions.append(of.ofp_action_output(port = of.OFFP_NORMAL))
+    fm.actions.append(of.ofp_action_output(port = of.OFPP_NORMAL))
     self.connection.send(fm)
 
     fm = of.ofp_flow_mod()
